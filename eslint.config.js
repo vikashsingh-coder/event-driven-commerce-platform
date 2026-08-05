1d82f0b4c7a2596a6e164989e48f153340511e6d
mentorvikash <mentorvikash@gmail.com>
2026-08-05 09:36:50 +0530

    chore: initialize monorepo structure

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    {
+        files: ["**/*.ts"],
+        extends: [
+            ...tseslint.configs.recommended
+        ],
+        rules: {
+            "@typescript-eslint/no-unused-vars": "error"
+        }
+    }
+);
